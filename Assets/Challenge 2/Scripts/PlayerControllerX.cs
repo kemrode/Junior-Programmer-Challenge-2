42c41c9b0d7abfe1b1b5e48f54bc3bb26540c776
Kemrode <kevinfichou@gmail.com>
2023-04-10 12:00:49 +0200

    feat(master): make the challenge 2 from Unity Junior Programmer

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float launchDog;
+    private const float timeBetweenTwoLaunches = 0.25f;
+
+    // Update is called once per frame
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(launchDog > 0)
+        {
+            launchDog -= Time.deltaTime;
+        }
+        LaunchDogOnSpacebarAction();
+    }
+
+    void LaunchDogOnSpacebarAction()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(launchDog <= 0)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                launchDog = timeBetweenTwoLaunches;
+            }
+        }
+    }
+}
